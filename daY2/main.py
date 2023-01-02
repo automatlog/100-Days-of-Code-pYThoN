62bf351b00b1c5b62d25142450170f2a896bbf3e
Aman Yadav <72098281+automatlog@users.noreply.github.com>
2023-01-02 19:25:41 +0530

    Create main.py

---
+++ daY2/main.py
@@ -0,0 +1,19 @@
+print("Getting to know you...!")
+
+#input
+myName = input("What's your name? : ")
+favFood = input("What's your favourite food? : ")
+favMusic = input("What's your favourite music? : ")
+myLocation = input("Where do you live? : ")
+
+print()  #line break
+
+#output
+print("You are")
+print(myName)
+print("You're probably hungry for")
+print(favFood)
+print("and you're definitely getting your groove on to")
+print(favMusic)
+print("living on the Planet Earth's country")
+print(myLocation)
