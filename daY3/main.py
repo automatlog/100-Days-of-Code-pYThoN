0cfbb82109a12dc192725e71c4830b5f8788806c
Aman Yadav <72098281+automatlog@users.noreply.github.com>
2023-01-03 23:24:41 +0530

    Create main.py

---
+++ daY3/main.py
@@ -0,0 +1,8 @@
+foodName = input("Name a food : ") #Mac & Cheese
+plantName = input("Name a type of plant : ") #Cactus
+cookingMethod = input("Name a method of cooking : ")  #Saute        
+describeWord = input("What word describes burned food? : ") #Ruined
+diyItemName = input("Name a DIY item : ") #Hammer
+
+print("\n---- MENU ----")
+print(cookingMethod, foodName, "with", describeWord, plantName,"on a bed of",diyItemName)
