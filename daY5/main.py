24905e384040157bff65cce4081f15161e436302
Aman Yadav <72098281+automatlog@users.noreply.github.com>
2023-01-08 03:26:36 +0530

    Create main.py

---
+++ daY5/main.py
@@ -0,0 +1,24 @@
+print("Marvel Movie Character Creator")
+
+print()
+hangingAround = input("Do you like 'hanging around'? : ")
+if hangingAround == "No":
+    print("Then you're not Spider-man")
+else:
+    print("Tada...! You are SpiderMan")
+
+
+print()
+gravellyVoice = input("Do you have a 'gravelly' voice? : ")
+if gravellyVoice == "No":
+    print("Aww, then you're not Korg")
+else:
+    print("oh... You are Korg")
+
+
+print()    
+marvelous = input("Do you often feel 'Marvelous'? : ")
+if marvelous == "Yes":
+    print("Aha! You're Captain Marvel! Hi!")
+else:
+    print("Oh no Try again next time...!")
