f2a58f466a6291ed0f12b1e165293a28a501505e
Aman Yadav <72098281+automatlog@users.noreply.github.com>
2023-01-07 23:26:48 +0530

    Create main.py

---
+++ day4/main.py
@@ -0,0 +1,14 @@
+print("\033[33m",'''      Welcome to your adventure simulator.''')
+print()
+print("\033[35m","I am going to ask you a bunch of questions and then create an epic story with you as the star...!","\033[0m")
+
+print()
+myName = input("What is your name? : ")
+myEnemyName = input("What is your worst enemy’s name? : ")
+mySuperpower = input("What is your superpower? : ")
+myLocation = input("Where do you live? : ")
+favFood = input("What is your favorite food? : ")
+
+print()
+print("\033[32m","Hello...!",myName ,"Your ability to", mySuperpower,"will make sure you never have to look at","\033[31m", myEnemyName ,"\033[32m","again.")
+print("Go eat",favFood," as you walk down the streets of",myLocation," and use",mySuperpower," for good and not evil...!")
