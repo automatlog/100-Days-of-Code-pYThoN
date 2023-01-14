684163b78bb8252dfe920f61791ce9cc152a3a22
Aman Yadav <72098281+automatlog@users.noreply.github.com>
2023-01-14 23:14:59 +0530

    Create main.py

---
+++ daY6/main.py
@@ -0,0 +1,17 @@
+print("MY LOGIN SYSTEM")
+print("+++++++++++++++")
+userName = input(" Username : ") 
+passWord = input(" Password : ") 
+
+if userName == "automatlog" and passWord =="Automat010":
+    print('''Hello there Aman, what a nice programming portfolio you have, add more everyday.
+Have a great day.
+Don't forget to wear a hat in the sun!''')
+elif userName == "johnathan" and passWord =="john56":
+    print('''Hello there Jonathan, what a lovely taste in music you have.
+Have a great day.''')
+elif userName == "Zainii" and passWord == "zaini89":
+    print('''Hello there Zainii, welcome to your art gallery.
+Have a great day.''')
+else:
+    print("Ummm Looks like you have forgotten your password, Try to contact Admin")
