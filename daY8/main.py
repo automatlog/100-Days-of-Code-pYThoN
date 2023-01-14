bd676e38a3286dbae8392b90140a8a753cd7b7ec
Aman Yadav <72098281+automatlog@users.noreply.github.com>
2023-01-14 23:16:28 +0530

    Create main.py

---
+++ daY8/main.py
@@ -0,0 +1,32 @@
+print("Hello. Welcome to your daily affirmation generator.")
+name = input("What is your name? : ")
+
+
+
+if name =="Aman" or name == "aman":
+ dayOfWeek = input("What is the day of the week? : ")
+ if dayOfWeek == "monday" or dayOfWeek == "Monday":
+   print("It is going to be a great Monday", name)
+ if dayOfWeek == "tuesday" or dayOfWeek == "Tuesday":
+   print("What a wonderful Tuesday it is", name)
+ if dayOfWeek == "wednesday" or dayOfWeek == "Wednesday":
+   print("Happy Hump Day", name)
+ if dayOfWeek == "thursday" or dayOfWeek == "Thursday":
+   print(name,"your week is almost over!")
+ if dayOfWeek == "friday" or dayOfWeek == "Friday":
+   print(name, "It's FRIDAY!")
+
+elif name == "David" or name== "david":
+ dayOfWeek = input("What is the day of the week?")
+ if dayOfWeek == "monday" or dayOfWeek == "Monday":
+   print("It is going to be a great Monday", name)
+ if dayOfWeek == "tuesday" or dayOfWeek == "Tuesday":
+   print("You look great in that color", name)
+ if dayOfWeek == "wednesday" or dayOfWeek == "Wednesday":
+   print("You look chipper today", name)
+ if dayOfWeek == "thursday" or dayOfWeek == "Thursday":
+   print(name,"you are doing a great job!")
+ if dayOfWeek == "friday" or dayOfWeek == "Friday":
+   print(name, "it's FRIDAY!")
+else:
+ print("I do not know your name, but I hope you are having a great day!")
